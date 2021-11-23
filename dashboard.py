7847777614c6db5da26a0de775d5e830d675164a
jtn243 <94773052+jtn243@users.noreply.github.com>
2021-11-23 06:32:18 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,4 @@
-from explainerdashboard import ClassifierExplainer, ExplainerDashboard
-
+from explainerdashboard import ClassifierExplainer, RegressionExplainer, ExplainerDashboard, ExplainerHub
 # you can override params during load from_config:
 db = ExplainerDashboard.from_config("dashboard.yaml", title="Awesomer Title")
 
